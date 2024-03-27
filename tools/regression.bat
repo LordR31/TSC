7ae281bf3a5a363febb4acf0cb745e426cae2f12
Andrei Visoiu <visoiuandre@gmail.com>
2024-03-27 09:39:17 +0200

    Laborator 6 - transcripts

---
+++ tools/regression.bat
@@ -0,0 +1,9 @@
+call run_test.bat 32 32 0 0 inc.inc c 
+call run_test.bat 32 32 0 1 inc.dec c 
+@REM call run_test.bat 32 32 1 2 inc.rdm c
+@REM call run_test.bat 32 32 1 0 dec.inc c
+@REM call run_test.bat 32 32 1 1 dec.dec c
+@REM call run_test.bat 32 32 1 2 dec.rnd c
+@REM call run_test.bat 32 32 2 0 rnd.inc c
+@REM call run_test.bat 32 32 2 1 rnd.dec c
+@REM call run_test.bat 32 32 2 2 rnd.rnd c
